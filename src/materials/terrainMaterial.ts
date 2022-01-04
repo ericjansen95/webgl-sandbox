dd4d0bff3418fc7463afa5461b35ada093c28bad
Eric Jansen <eric.jansen@study.hs-duesseldorf.de>
2022-01-04 02:01:26 +0100

    Add basic errosion map calculation on cpu

--- src/materials/terrainMaterial.ts
+++ src/materials/terrainMaterial.ts
@@ -11,6 +11,7 @@ export default class TerrainMaterial implements Material {
   attributeLocations: Map<string, number>
   uniformLocations: Map<string, WebGLUniformLocation>
   heightmap: WebGLTexture
+  errosionMap: WebGLTexture
 
   constructor(renderer: Renderer, heightmapUri: string) {
     this.type = "TERRAIN"
@@ -26,21 +27,117 @@ export default class TerrainMaterial implements Material {
 
     this.uniformLocations.set('uTexture', renderer.gl.getUniformLocation(program, 'uTexture'))
 
-    this.heightmap = renderer.gl.createTexture();
-    renderer.gl.bindTexture(renderer.gl.TEXTURE_2D, this.heightmap);
+    this.heightmap = renderer.gl.createTexture()
+    renderer.gl.bindTexture(renderer.gl.TEXTURE_2D, this.heightmap)
 
     // ToDo(Eric) Manage texture level / position!
     renderer.gl.texImage2D(renderer.gl.TEXTURE_2D, 0, renderer.gl.RGBA, 1, 1, 0, renderer.gl.RGBA, renderer.gl.UNSIGNED_BYTE,
-                            new Uint8Array([0, 0, 255, 255]));
+                            new Uint8Array([0, 0, 255, 255]))
       
-    var image = new Image();
-    image.src = heightmapUri;
+    const heightmap: HTMLImageElement = new Image()
+    heightmap.src = heightmapUri
 
-    image.addEventListener('load', () => {
+    heightmap.addEventListener('load', () => {
       renderer.gl.bindTexture(renderer.gl.TEXTURE_2D, this.heightmap);
-      renderer.gl.texImage2D(renderer.gl.TEXTURE_2D, 0, renderer.gl.RGBA, renderer.gl.RGBA, renderer.gl.UNSIGNED_BYTE, image);
+      renderer.gl.texImage2D(renderer.gl.TEXTURE_2D, 0, renderer.gl.RGBA, renderer.gl.RGBA, renderer.gl.UNSIGNED_BYTE, heightmap);
       renderer.gl.generateMipmap(renderer.gl.TEXTURE_2D);
+
+      this.createErrosionMap(heightmap)
     });
+  }
+
+  createErrosionMap = (heightmap: HTMLImageElement): WebGLTexture | null => {
+    if(!heightmap) return null
+
+    const canvasErrosion: HTMLCanvasElement = document.createElement('canvas')
+
+    canvasErrosion.width = heightmap.width
+    canvasErrosion.height = heightmap.height
+
+    const canvasErroision2dContext = canvasErrosion.getContext('2d')
+
+    canvasErroision2dContext.beginPath();
+    canvasErroision2dContext.rect(0, 0, heightmap.height, heightmap.width);
+    canvasErroision2dContext.fillStyle = "transparent";
+    canvasErroision2dContext.fill();
+
+    canvasErrosion.style.cssText = 'position: absolute; width: 256px; height: 256px; right: 0px; top: 0px; z-index: 300;'
+    document.body.appendChild(canvasErrosion)
+
+    const canvasHeightmap: HTMLCanvasElement = document.createElement('canvas')
+
+    canvasHeightmap.width = heightmap.width
+    canvasHeightmap.height = heightmap.height
+
+    canvasHeightmap.getContext('2d').drawImage(heightmap, 0, 0, heightmap.width, heightmap.height);
+
+    canvasHeightmap.style.cssText = 'position: absolute; width: 256px; height: 256px; right: 0px; top: 0px; z-index: 200;'
+    document.body.appendChild(canvasHeightmap)
+
+    const MIN_ERROSION_TRAIN_COUNT: number = 64
+    let errosionTrailCount: number = 0
+
+    const MIN_START_HEIGHT = Math.ceil(255 * 0.25)
+
+    const pixelData: Uint8ClampedArray = new Uint8ClampedArray([255, 0, 0, 255])
+    const imageData: ImageData = new ImageData(pixelData, 1.0)
+
+    while(errosionTrailCount < MIN_ERROSION_TRAIN_COUNT) {
+      let height: number = 0.0
+
+      let xPos: number = 0.0
+      let yPos: number = 0.0
+  
+      while(height <= MIN_START_HEIGHT) {
+        xPos = Math.ceil(Math.random() * heightmap.height)
+        yPos = Math.ceil(Math.random() * heightmap.width)
+    
+        height = canvasHeightmap.getContext('2d').getImageData(xPos, yPos, 1, 1).data[0]
+      }
+  
+      //console.log(`Start position = [${xPos}, ${yPos}]`)
+      //console.log(`Height at start position = ${height}`)
+  
+      let prevHeight: number = height + 1
+
+      const positions: Array<{x: number, y: number}> = new Array<{x: number, y: number}>()
+
+      let iterration: number = 0
+
+      let deltaHeightX: number = 1
+      let deltaHeightY: number = 1
+
+      while(height < prevHeight && height > 1 && (deltaHeightX !== 0 || deltaHeightY !== 0)) {
+        const heightLeft: number = canvasHeightmap.getContext('2d').getImageData(xPos + 1, yPos, 1, 1).data[0]
+        const heightTop: number = canvasHeightmap.getContext('2d').getImageData(xPos, yPos + 1, 1, 1).data[0]
+        const heightRight: number = canvasHeightmap.getContext('2d').getImageData(xPos - 1, yPos, 1, 1).data[0]
+        const heightBottom: number = canvasHeightmap.getContext('2d').getImageData(xPos, yPos - 1, 1, 1).data[0]
+  
+        deltaHeightX = heightLeft - heightRight
+        deltaHeightY = heightTop - heightBottom
+  
+        // console.log(`Delta height = [${deltaHeightX}, ${deltaHeightY}]`)
+
+        if(Math.abs(deltaHeightY) <= Math.abs(deltaHeightX))
+          xPos -= Math.sign(deltaHeightX)
+        else
+          yPos -= Math.sign(deltaHeightY)
+  
+        prevHeight = height
+        height = canvasHeightmap.getContext('2d').getImageData(xPos, yPos, 1, 1).data[0]
+
+        // console.log(`Previous height = ${prevHeight} | Current height = ${height}`)
+        // console.log(`Itertation = ${++iterration} at position = [${xPos}, ${yPos}] with height = ${height}`)
+        
+        positions.push({x: xPos, y: yPos})
+      }
+
+      if(positions.length > 4) {
+        errosionTrailCount++
+        console.log(`Errosion trails ${errosionTrailCount} / ${MIN_ERROSION_TRAIN_COUNT}`)
+        positions.forEach(position => canvasErrosion.getContext('2d').putImageData(imageData, position.x, position.y))
+      }
+    }
   }
 
   bind = (gl: WebGL2RenderingContext, lightDir: vec3, textureLocation: number) => {
