2d58096390922226dd2dc9f3602eec4d1c359c4c
Eric Jansen <eric.jansen@study.hs-duesseldorf.de>
2022-06-18 21:40:15 +0200

    Remove code dublicaion in gltf loader

--- src/core/loader/gltfLoader.ts
+++ src/core/loader/gltfLoader.ts
@@ -4,115 +4,85 @@ export type GlftLoadResponse = {
   geometry: Array<Geometry>
 }
 
+enum ComponentType {
+  VEC3 = 3,
+  VEC2 = 2,
+  SCALAR = 1
+}
+
 const parseGeometry = async (gltf: any, bufferData: Array<ArrayBuffer>): Promise<Array<Geometry>> => {
   const { meshes, accessors, bufferViews } = gltf
   const geometries = new Array<Geometry>()
+
+  const getBufferViewFromAccessorIndex = (accessorIndex: number, component: ComponentType) => {
+    const accessorEntry = accessors[accessorIndex]
+    const {
+      bufferView,
+      componentType,
+      count,
+      type,
+    } = accessorEntry
+
+    const bufferViewEntry = bufferViews[bufferView]
+    const {
+      buffer,
+      byteLength,
+      byteOffset,
+    } = bufferViewEntry
+
+    switch(component) {
+      case ComponentType.VEC3:
+        return new Float32Array(bufferData[buffer], byteOffset, count * 3)
+      case ComponentType.VEC2:
+        return new Float32Array(bufferData[buffer], byteOffset, count * 2)
+      case ComponentType.SCALAR:
+        return new Uint16Array(bufferData[buffer], byteOffset, count)
+    }    
+  }
 
   for(const mesh of meshes) {
     const { primitives, name } = mesh
 
     for(const primitive of primitives) {
-      const { attributes, indices } = primitive
-
-      const indiciesAccessor = accessors[indices]
-      const {
-        bufferView,
-        componentType,
-        count,
-        type,
-      } = indiciesAccessor
-
-      const indiciesBufferView = bufferViews[bufferView]
-      const {
-        buffer,
-        byteLength,
-        byteOffset,
-      } = indiciesBufferView
-
-      const indiciesArray = new Uint16Array(bufferData[buffer], byteOffset, count)
+      const { attributes } = primitive
 
       const geometry = new Geometry()
-      let positions = null
-      let normals = null
-      let uvs = null
+
+      const indicesAccessorIndex = primitive.indices as number
+      const indices = getBufferViewFromAccessorIndex(indicesAccessorIndex, ComponentType.SCALAR) as Uint16Array
+
+      let position = null
+      let normal = null
+
+      let texcoord = null
 
       for(const [key, value] of Object.entries(attributes as object)) {
         switch (key) {
           case "POSITION": {
             const positionAccessorIndex = value as number
-
-            const positionAccessor = accessors[positionAccessorIndex]
-            const {
-              bufferView,
-              componentType,
-              count,
-              type,
-            } = positionAccessor
-
-            const positionBufferView = bufferViews[bufferView]
-            const {
-              buffer,
-              byteLength,
-              byteOffset,
-            } = positionBufferView
-
-            positions = new Float32Array(bufferData[buffer], byteOffset, count * 3)
-
+            position = getBufferViewFromAccessorIndex(positionAccessorIndex, ComponentType.VEC3)
             break
           }
           case "NORMAL": {
             const normalAccessorIndex = value as number
-
-            const normalAccessor = accessors[normalAccessorIndex]
-            const {
-              bufferView,
-              componentType,
-              count,
-              type,
-            } = normalAccessor
-
-            const normalBufferView = bufferViews[bufferView]
-            const {
-              buffer,
-              byteLength,
-              byteOffset,
-            } = normalBufferView
-
-            normals = new Float32Array(bufferData[buffer], byteOffset, count * 3)
-
+            normal = getBufferViewFromAccessorIndex(normalAccessorIndex, ComponentType.VEC3)
             break
           }
           case "TEXCOORD_0": {
-            const uvAccessorIndex = value as number
-
-            const uvAccessor = accessors[uvAccessorIndex]
-            const {
-              bufferView,
-              componentType,
-              count,
-              type,
-            } = uvAccessor
-
-            const uvBufferView = bufferViews[bufferView]
-            const {
-              buffer,
-              byteLength,
-              byteOffset,
-            } = uvBufferView
-
-            uvs = new Float32Array(bufferData[buffer], byteOffset, count * 2)
-
+            const texcoordAccessorIndex = value as number
+            texcoord = getBufferViewFromAccessorIndex(texcoordAccessorIndex, ComponentType.VEC2)
             break
           }
         }
       }
 
       geometry.setVertices({
-        indices: indiciesArray,
-        position: positions, 
-        normal: normals, 
-        texcoord: uvs
+        indices,
+        position, 
+        normal, 
+        texcoord
       })
+      
       geometries.push(geometry)
     }
   }
